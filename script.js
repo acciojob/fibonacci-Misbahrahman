ff409418e1a2d206f868d43a2252b2ffd159a61e
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-11-30 22:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+    let i = 0;
+	let j = 1;
+	while(num != 0){
+		let k = i + j;
+		j = k;
+		i = j;
+
+		num--;
+		
+	}
+
+	return j;
+
+	
 }
 
 module.exports = fibonacci;
